42e93bd3b09a0fb522f8fceb77d401d6b5543d0e
Garrett Brown <gdbrown09@gmail.com>
2021-05-11 19:20:01 -0500

    Remebering how to python

--- environment/botRestart.py
+++ environment/botRestart.py
@@ -1,7 +1,6 @@
 import os
 
-if(os.system('pm2 pid "randomAcro')){
-    os.system('pm2 restart "rancomdAcro');
-} else{
-    os.system('pm2 start "rancomdAcro');
-}
+if os.system('pm2 pid "randomAcro')):
+    os.system('pm2 restart "rancomdAcro')
+else"
+    os.system('pm2 start "rancomdAcro')
