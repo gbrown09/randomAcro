a64dcb300c884ea8e07acf200d4d8318e44ff0a9
Garrett Brown <gdbrown09@gmail.com>
2021-05-11 19:27:53 -0500

    Typos

--- environment/botRestart.py
+++ environment/botRestart.py
@@ -1,6 +1,6 @@
 import os
 
-if os.system('pm2 pid "randomAcro')):
+if os.system('pm2 pid "randomAcro'):
     os.system('pm2 restart "rancomdAcro')
-else"
+else:
     os.system('pm2 start "rancomdAcro')
