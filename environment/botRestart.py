f4a188d77de06f19b5e89821968102326bfae268
Garrett Brown <gdbrown09@gmail.com>
2021-05-11 19:28:56 -0500

    why am i so dumb

--- environment/botRestart.py
+++ environment/botRestart.py
@@ -1,6 +1,6 @@
 import os
 
 if os.system('pm2 pid "randomAcro'):
-    os.system('pm2 restart "rancomdAcro')
+    os.system('pm2 restart "rancomdAcro"')
 else:
-    os.system('pm2 start "rancomdAcro')
+    os.system('pm2 start "rancomdAcro"')
